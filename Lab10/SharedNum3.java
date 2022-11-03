4ee6ea80d43852069d8684aafaeee9cc2b625f64
Emotions <estskywalker@gmail.com>
2022-11-03 14:50:55 +0700

    finish

---
+++ Lab10/SharedNum3.java
@@ -0,0 +1,12 @@
+public class SharedNum3 { // synchronized method
+    private int val = 0;
+
+    // SharedNum3() { val = 0; }
+    synchronized void increment() {
+        val++;
+    }
+
+    int getVal() {
+        return val;
+    }
+}
