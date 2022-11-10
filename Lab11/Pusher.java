fde3e703be1f7e2431836f16d0a83fd0c107fc73
Emotions <estskywalker@gmail.com>
2022-11-10 15:10:01 +0700

    push lab11 with material

---
+++ Lab11/Pusher.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+class Pusher extends Thread {
+    int turns;
+    StackForConcurrent concurStack;
+
+    Pusher(int t, StackForConcurrent s) {
+        turns = t;
+        concurStack = s;
+    }
+
+    // constructor
+    /* Q5 */ 
+    public void run() {
+        Random rand = new Random();
+        try {
+            sleep(rand.nextInt(100));
+        } catch (InterruptedException ie) {
+        }
+        for (int i = 0; i < turns; i++)
+            concurStack.push(i);
+    } // run
+}
